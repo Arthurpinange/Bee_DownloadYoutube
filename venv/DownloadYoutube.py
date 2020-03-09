92bf1a557b9cd5a8932e194f8cecd686b9a0bd8a
arthurpinange <arthur.pinange@gmail.com>
2020-03-09 18:22:38 -0300

    Commit Inicial do Bee

---
+++ venv/DownloadYoutube.py
@@ -0,0 +1,53 @@
+from pytube import YouTube
+from pathlib import Path
+class Download:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def DownloadFile(url,path):
+        '''
+        performs the video download and returns the file path
+        :param url:
+        :param path:
+        :return path :
+        '''
+        try:
+            if Path(path).exists():
+                yt = YouTube(url)
+                video = yt.streams.first()
+                path = video.download(path)
+                return path
+            else:
+                return "Error: Path not exists"
+        except:
+            return "Error: check the url unknown"
+
+    @staticmethod
+    def DownloadPlayList(urlplaylist,path):
+        '''
+        download videos from a playlist
+        :param urlplaylist:
+        :param path:
+        :return:
+        '''
+        playlist = Playlist(urlplaylist)
+        if len(playlist) >0:
+            if Path(path).exists():
+                try:
+                    for url in playlist:
+                        yt = YouTube(url)
+                        video = yt.streams.first()
+                        video.download(path)
+                    return path
+                except:
+                    return "Error: check the url unknown"
+            else:
+                return "Error: Path not exists"
+        else:
+            return "Error: no video was listed in the playlist"
+
+    @staticmethod
+    def teste():
+        print('Jesus tu esta comigo ! sara meu coraçao !')
---
+++ venv/Youtube.py
@@ -0,0 +1,36 @@
+from DownloadYoutube import Download
+import sys
+
+class __main__:
+
+    def __init__(self):
+        pass
+
+    if __name__ == "__main__":
+        print('Seja bem vindo viajante do tempo !')
+        print('Digite as seguintes argumentos:')
+        print('tipoDownload url path')
+        idx = 1
+        if len(sys.argv) > 1:
+            for p in sys.argv:
+                if idx == 2:
+                    tipo = p
+                elif idx == 3:
+                    url = p
+                elif idx == 4:
+                    path = p
+                idx = idx + 1
+        else:
+            tipo = input('Tipo de Download (video ou playlist): ')
+            url = input('informe a url: ')
+            path = input('informe a caminho: ')
+
+        if tipo == "video":
+            result = Download.DownloadFile(url, path)
+            print(result)
+        elif tipo == "playlist":
+            result = Download.DownloadPlayList(url, path)
+            print(result)
+        else:
+            print("Informe um dos tipos video ou playlist")
+        print(len(sys.argv))
